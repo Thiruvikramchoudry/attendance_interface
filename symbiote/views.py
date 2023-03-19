5c401292070619a11538cf6c39ed8ff7d708c675
vikram <thir20ad050@rmkcet.ac.in>
2023-03-19 19:45:44 +0530

    no idea

--- symbiote/views.py
+++ symbiote/views.py
@@ -10,7 +10,37 @@ import os
 # Create your views here.
 
 def main(request):
-    return render(request,'symbiote/login.html')
+    data = attendence_area.objects.all()[:5]
+    total_count = len(details.objects.all())
+    date = datetime.date.today()
+    today_count = len(attendence_area.objects.filter(Date=date))
+    today_emp = attendence_area.objects.filter(Date=date)
+    late_entry = 0
+    for i in today_emp:
+        time = str((i.Time)).split(":")
+        print(time)
+        hh = int(time[0])
+        mm = int(time[1])
+        ss = int(time[2])
+        if hh > 10 or (hh == 10 and mm != 00):
+            late_entry += 1
+    record = absenteism_count.objects.all()
+    present_count = [];
+    total_person = [];
+    late_person = [];
+    preleave_person = [];
+    dates = []
+    for i in record[::-1][:6][::-1]:
+        present_count.append(i.Total_person - i.preleave_count - i.absent_count)
+        total_person.append(i.Total_person)
+        late_person.append(i.late_count)
+        preleave_person.append(i.preleave_count)
+        dates.append(i.Date)
+
+    return render(request, 'symbiote/index.html',
+                  {'username': "sample", 'details': data, 'total_count': total_count, 'today_count': today_count,
+                   'late_entry': late_entry, 'present_count': present_count, 'total_person': total_person,
+                   'late_person': late_person, 'preleave_person': preleave_person, 'dates': dates})
 
 
 def login(request):
