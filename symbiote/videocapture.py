f27377078aec707d3dd22973e4f406ec166a1f06
vikram <thir20ad050@rmkcet.ac.in>
2023-04-23 23:09:39 +0530

    home page added ...video capture added and edited...supervisor features added

---
+++ symbiote/videocapture.py
@@ -0,0 +1,23 @@
+import cv2
+from django.http import StreamingHttpResponse
+
+
+class VideoCamera:
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        ret, frame = self.video.read()
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tostring()
+
+    def stop_streaming(self):
+         self.video.release()
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
--- symbiote/views.py
+++ symbiote/views.py
@@ -8,14 +8,18 @@ import pandas as pd
 import symbiote.main_db_connection as mdb
 import os
 import cv2
-
+from symbiote.videocapture import VideoCamera,gen
+from django.http import StreamingHttpResponse
 
 
 
 # Create your views here.
 
-def main(request,username):
-    project_id = supervisor_assign.objects.get(supervisor_username=username).project_id
+def index(request):
+    return render(request,'symbiote/home_page.html')
+
+def main(request):
+    project_id = supervisor_assign.objects.get(supervisor_username=request.user).project_id
     employee_count = project.objects.get(project_id=project_id).employee_required
     today = datetime.date.today()
 
@@ -32,7 +36,7 @@ def main(request,username):
     for i in range(min(5, len(detail_name))):
         details.append([emp_id[i], detail_name[i], detail_status[i]])
 
-    return render(request, 'symbiote/index.html',{'username': username, 'employee_count': employee_count, 'present_count': status, 'details': details})
+    return render(request, 'symbiote/index.html',{'username': request.user, 'employee_count': employee_count, 'present_count': status, 'details': details})
 
 
 
@@ -50,7 +54,42 @@ def login(request):
         return render(request,'symbiote/login.html', {'message': ""})
 
 def admin_page(request):
-    return render(request,'symbiote/admin_page.html')
+    supervisor_count=len(supervisor_detail.objects.all())
+    project_count=len(project.objects.all())
+    employee_count=len(employee_details.objects.all())
+    employee_required=0
+    for i in project.objects.all():
+        employee_required+=int(i.employee_required)
+    projects=project.objects.all()
+
+
+
+    return render(request,'symbiote/admin_page.html',{"username":request.user,'supervisor_count':supervisor_count,'project_count':project_count,'employee_required':employee_count-employee_required,'total_employee':employee_count,'projects':projects})
+
+
+def project_details(request):
+    projects=project.objects.all()
+    return render(request,'symbiote/project_details.html',{'username':request.user,'projects':projects})
+
+def employee_detail_all(request):
+    details=employee_details.objects.all()
+
+    return render(request,'symbiote/employee_detail_all.html',{'details':details,'username':request.user})
+
+
+def new_project(request):
+    supervisor=supervisor_detail.objects.all()
+    supervisor_available=[]
+    for i in supervisor:
+        if i.status==False:
+            supervisor_available.append(i)
+
+
+    if request.method=="POST":
+        pass
+    else:
+        return render(request,'symbiote/new_project.html',{''})
+
 
 
 def sample(request):
@@ -87,18 +126,18 @@ def clear(request):
     mdb.delete_attendence()
     return redirect('/')
 
-def employee_detail(request,username):
-    project_id=supervisor_assign.objects.get(supervisor_username=username).project_id
+def employee_detail(request):
+    project_id=supervisor_assign.objects.get(supervisor_username=request.user).project_id
     Employee_id=employee_assign.objects.filter(project_id=project_id)
     employee_detail=[]
     for i in Employee_id:
         employee_detail.append(employee_details.objects.get(employee_id=i.employee_id))
 
-    return render(request,'symbiote/employee_details.html',{'username':username,'details':employee_detail})
+    return render(request,'symbiote/employee_details.html',{'username':request.user,'details':employee_detail})
 
 
-def attendance_status(request,username):
-    project_id = supervisor_assign.objects.get(supervisor_username=username).project_id
+def attendance_status(request):
+    project_id = supervisor_assign.objects.get(supervisor_username=request.user).project_id
     today=datetime.date.today()
     file_name = (str(today.day) + '-' + str(today.month if len(str(today.month)) == 2 else '0' + str(today.month)) + '-' + str(today.year)) + ".xlsx"
     df = pd.read_excel(r'symbiote/employee_assign/' + str(project_id) + '/' + file_name)
@@ -108,25 +147,22 @@ def attendance_status(request,username):
     details = []
     for i in range( len(detail_name)):
         details.append([emp_id[i], detail_name[i], detail_status[i]])
-    return render(request,'symbiote/attendance_status.html',{'username': username,'details':details})
+    return render(request,'symbiote/attendance_status.html',{'username': request.user,'details':details})
 
 
 def add_employee(request):
-    data = attendence_area.objects.all()
-    if request.method=="POST":
-        name=request.POST['name']
-        designation=request.POST['designation']
-        date_of_birth=request.POST['date_of_birth']
-        gender= request.POST['gender']
-        address = request.POST['address']
-        phone=request.POST['phone_number']
-        image = request.POST.get("image")
-        # emp=details(name=name,designation=designation,date_of_birth=date_of_birth,gender=gender,address=address,phone=phone)
-        # emp.save()
-        print(name,designation,date_of_birth,gender,address,phone,image)
-        return redirect('sample')
 
-    return render(request,'symbiote/add_employee.html',{'username':request.user, 'details': data})
+    if request.method=="POST":
+        name = request.POST['name']
+        age = request.POST['age']
+        date_of_birth = request.POST['date_of_birth']
+        gender = request.POST['gender']
+        address = request.POST['address']
+        phone = request.POST['phone_number']
+        aadhar_number = request.POST['aadhar_number']
+        image = request.POST.get("image")
+
+    return render(request,'symbiote/add_employee.html')
 
 
 
@@ -204,14 +240,23 @@ def save_clear(request):
 
 
 def download_stats(request):
-    if request.method=="POST":
-        EMP_ID= request.POST['emp_ID']
-        detail=attendence_area(employee_id=EMP_ID)
-        mdb.add(EMP_ID)
-        detail.save()
-        return redirect('download_stats')
-
     return render(request,'symbiote/download_files.html',{"username":request.user})
+
+def video_feed(request):
+    return StreamingHttpResponse(gen(VideoCamera()), content_type='multipart/x-mixed-replace; boundary=frame')
+
+def stop_streaming(request):
+    if request.method=="POST":
+        print("PRINTING")
+        camera = VideoCamera()
+        camera.stop_streaming()
+        return render(request,'symbiote/download_files.html' , {"status": True , "username":request.user})
+    return render(request, 'symbiote/download_files.html' , {"status": False , "username":request.user})
+
+def uncapture(request):
+    if request.method=="POST":
+       return redirect('stop_streaming')
+
 
 def login2(request):
     if request.method=="POST":
@@ -233,16 +278,19 @@ def login_supervisor(request):
     if request.method=="POST":
         username=request.POST["username"]
         password=request.POST["password"]
-        if len(supervisor_detail.objects.filter(username=username,password=password))!=0:
-            if len(supervisor_assign.objects.filter(supervisor_username=username)) != 0:
-                return redirect('supervisor_page/'+username)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            if len(supervisor_detail.objects.filter(username=username,password=password))!=0:
+                if len(supervisor_assign.objects.filter(supervisor_username=username)) != 0:
+                    return redirect('supervisor_page')
+                else:
+                    return render(request, 'symbiote/no_assign.html',{'username':username})
+
             else:
-                return render(request, 'symbiote/no_assign.html',{'username':username})
+                return render(request, 'symbiote/login2.html', {'message': 'Incorrect Credientials'})
 
-        else:
-            print("no account")
-
-            return render(request,'symbiote/login2.html',{'message':'Incorrect Credientials'})
+        return render(request,'symbiote/login2.html',{'message':'Incorrect Credientials'})
     else:
         return render(request,'symbiote/login2.html',{'message':''})
 
