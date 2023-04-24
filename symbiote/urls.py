1037ad6fc033dee6714a9f66608084dbcb913a14
srinath0307 <srinathmurali2003@gmail.com>
2023-04-24 18:18:21 +0530

    bug fix

--- symbiote/urls.py
+++ symbiote/urls.py
@@ -1,32 +1,29 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.index),
-    path('home',views.main,name="home"),
+    path('', views.index),
+    path('home', views.main, name="home"),
     path('login_admin', views.login, name="login_admin"),
     # path('sample',views.sample,name="sample"),
     # path('clear_data',views.clear,name="clear_data"),
-    path('employee_details',views.employee_detail,name="employee_details"),
-    path('attendance_status',views.attendance_status,name="attendance_status"),
-    path('add_employee',views.add_employee,name="add_employee"),
+    path('employee_details', views.employee_detail, name="employee_details"),
+    path('attendance_status', views.attendance_status, name="attendance_status"),
+    path('add_employee', views.add_employee, name="add_employee"),
     # path('save_data',views.save_excel,name="save_data"),
     # path('save_clean',views.save_clear,name="save_clean"),
-    path('download_stats',views.download_stats,name="download_stats"),
-    path('login2',views.login2,name="login2"),
-    path('logout',views.logout,name="logout"),
-    path('login_supervisor',views.login_supervisor,name="login_supervisor"),
-    path('admin_page',views.admin_page,name="admin_page"),
-    path('supervisor_page',views.main,name="supervisor_page"),
-    path('project_details',views.project_details,name="project_details"),
-    path('employee_detail_all',views.employee_detail_all,name="employee_detail_all"),
-    path('new_project',views.new_project,name="new_project"),
+    path('download_stats', views.download_stats, name="download_stats"),
+    path('login2', views.login2, name="login2"),
+    path('logout', views.logout, name="logout"),
+    path('login_supervisor', views.login_supervisor, name="login_supervisor"),
+    path('admin_page', views.admin_page, name="admin_page"),
+    path('supervisor_page', views.main, name="supervisor_page"),
+    path('project_details', views.project_details, name="project_details"),
+    path('employee_detail_all', views.employee_detail_all, name="employee_detail_all"),
+    path('new_project', views.new_project, name="new_project"),
     path('video_feed', views.video_feed, name='video_feed'),
     path('stop_streaming', views.stop_streaming, name='stop_streaming'),
     path('uncapture', views.uncapture, name='uncapture')
-
-
 
 ]
--- symbiote/views.py
+++ symbiote/views.py
@@ -1,29 +1,30 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib.auth import authenticate
-#from .models import details,attendence_area,absenteism_count
-from .models import supervisor_detail,supervisor_assign,project,employee_details,employee_assign
-import datetime,json
+# from .models import details,attendence_area,absenteism_count
+from .models import supervisor_detail, supervisor_assign, project, employee_details, employee_assign
+import datetime, json
 import pandas as pd
-#import symbiote.main_db_connection as mdb
+# import symbiote.main_db_connection as mdb
 import os
 import cv2
-from symbiote.videocapture import VideoCamera,gen
+from symbiote.videocapture import VideoCamera, gen
 from django.http import StreamingHttpResponse
-
 
 
 # Create your views here.
 
 def index(request):
-    return render(request,'symbiote/home_page.html')
+    return render(request, 'symbiote/home_page.html')
+
 
 def main(request):
     project_id = supervisor_assign.objects.get(supervisor_username=request.user).project_id
     employee_count = project.objects.get(project_id=project_id).employee_required
     today = datetime.date.today()
 
-    file_name = (str(today.day) + '-' + str(today.month if len(str(today.month)) == 2 else '0' + str(today.month)) + '-' + str(today.year)) + ".xlsx"
+    file_name = (str(today.day) + '-' + str(
+        today.month if len(str(today.month)) == 2 else '0' + str(today.month)) + '-' + str(today.year)) + ".xlsx"
     df = pd.read_excel(r'symbiote/employee_assign/' + str(project_id) + '/' + file_name)
     emp_id = df['Employee_id']
     try:
@@ -36,14 +37,15 @@ def main(request):
     for i in range(min(5, len(detail_name))):
         details.append([emp_id[i], detail_name[i], detail_status[i]])
 
-    return render(request, 'symbiote/index.html',{'username': request.user, 'employee_count': employee_count, 'present_count': status, 'details': details})
-
+    return render(request, 'symbiote/index.html',
+                  {'username': request.user, 'employee_count': employee_count, 'present_count': status,
+                   'details': details})
 
 
 def login(request):
-    if request.method=="POST":
-        username=request.POST['username']
-        password=request.POST['password']
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
         user = authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user)
@@ -51,108 +53,109 @@ def login(request):
         else:
             return render(request, 'symbiote/login.html', {'message': "Invalid credientials"})
     else:
-        return render(request,'symbiote/login.html', {'message': ""})
+        return render(request, 'symbiote/login.html', {'message': ""})
+
 
 def admin_page(request):
-    supervisor_count=len(supervisor_detail.objects.all())
-    project_count=len(project.objects.all())
-    employee_count=len(employee_details.objects.all())
-    employee_required=0
+    supervisor_count = len(supervisor_detail.objects.all())
+    project_count = len(project.objects.all())
+    employee_count = len(employee_details.objects.all())
+    employee_required = 0
     for i in project.objects.all():
-        employee_required+=int(i.employee_required)
-    projects=project.objects.all()
+        employee_required += int(i.employee_required)
+    projects = project.objects.all()
 
-
-
-    return render(request,'symbiote/admin_page.html',{"username":request.user,'supervisor_count':supervisor_count,'project_count':project_count,'employee_required':employee_count-employee_required,'total_employee':employee_count,'projects':projects})
+    return render(request, 'symbiote/admin_page.html',
+                  {"username": request.user, 'supervisor_count': supervisor_count, 'project_count': project_count,
+                   'employee_required': employee_count - employee_required, 'total_employee': employee_count,
+                   'projects': projects})
 
 
 def project_details(request):
-    projects=project.objects.all()
-    return render(request,'symbiote/project_details.html',{'username':request.user,'projects':projects})
+    projects = project.objects.all()
+    return render(request, 'symbiote/project_details.html', {'username': request.user, 'projects': projects})
+
 
 def employee_detail_all(request):
-    details=employee_details.objects.all()
+    details = employee_details.objects.all()
 
-    return render(request,'symbiote/employee_detail_all.html',{'details':details,'username':request.user})
+    return render(request, 'symbiote/employee_detail_all.html', {'details': details, 'username': request.user})
 
 
 def new_project(request):
-    supervisor=supervisor_detail.objects.all()
-    supervisor_available=[]
+    supervisor = supervisor_detail.objects.all()
+    supervisor_available = []
     for i in supervisor:
-        if i.status==False:
+        if i.status == False:
             supervisor_available.append(i)
 
-
-    if request.method=="POST":
+    if request.method == "POST":
         pass
     else:
-        return render(request,'symbiote/new_project.html',{''})
+        return render(request, 'symbiote/new_project.html', {''})
 
 
-
-def sample(request):
-    data = attendence_area.objects.all()[:5]
-    total_count=len(details.objects.all())
-    date=datetime.date.today()
-    today_count=len(attendence_area.objects.filter(date=date))
-    today_emp=attendence_area.objects.filter(date=date)
-    late_entry=0
-    for i in today_emp:
-        time=str((i.Time)).split(":")
-        print(time)
-        hh=int(time[0])
-        mm=int(time[1])
-        if hh>10 or (hh==10 and mm!=00 ):
-            late_entry+=1
-    record=absenteism_count.objects.all()
-    present_count=[];total_person=[];late_person=[];preleave_person=[];dates=[]
-    for i in record[::-1][:6][::-1]:
-        present_count.append(i.Total_person-i.preleave_count-i.absent_count)
-        total_person.append(i.Total_person)
-        late_person.append(i.late_count)
-        preleave_person.append(i.preleave_count)
-        dates.append(i.date)
-
-
-    return render(request, 'symbiote/index.html', {'username': request.user, 'details': data,'total_count':total_count,'today_count':today_count,'late_entry':late_entry,'present_count':present_count,'total_person':total_person,'late_person':late_person,'preleave_person':preleave_person,'dates':dates})
-
-
-
-def clear(request):
-    data=attendence_area.objects.all()
-    data.delete()
-    mdb.delete_attendence()
-    return redirect('/')
+# def sample(request):
+#     data = attendence_area.objects.all()[:5]
+#     total_count=len(details.objects.all())
+#     date=datetime.date.today()
+#     today_count=len(attendence_area.objects.filter(date=date))
+#     today_emp=attendence_area.objects.filter(date=date)
+#     late_entry=0
+#     for i in today_emp:
+#         time=str((i.Time)).split(":")
+#         print(time)
+#         hh=int(time[0])
+#         mm=int(time[1])
+#         if hh>10 or (hh==10 and mm!=00 ):
+#             late_entry+=1
+#     record=absenteism_count.objects.all()
+#     present_count=[];total_person=[];late_person=[];preleave_person=[];dates=[]
+#     for i in record[::-1][:6][::-1]:
+#         present_count.append(i.Total_person-i.preleave_count-i.absent_count)
+#         total_person.append(i.Total_person)
+#         late_person.append(i.late_count)
+#         preleave_person.append(i.preleave_count)
+#         dates.append(i.date)
+#
+#
+#     return render(request, 'symbiote/index.html', {'username': request.user, 'details': data,'total_count':total_count,'today_count':today_count,'late_entry':late_entry,'present_count':present_count,'total_person':total_person,'late_person':late_person,'preleave_person':preleave_person,'dates':dates})
+#
+#
+#
+# def clear(request):
+#     data=attendence_area.objects.all()
+#     data.delete()
+#     mdb.delete_attendence()
+#     return redirect('/')
 
 def employee_detail(request):
-    project_id=supervisor_assign.objects.get(supervisor_username=request.user).project_id
-    Employee_id=employee_assign.objects.filter(project_id=project_id)
-    employee_detail=[]
+    project_id = supervisor_assign.objects.get(supervisor_username=request.user).project_id
+    Employee_id = employee_assign.objects.filter(project_id=project_id)
+    employee_detail = []
     for i in Employee_id:
         employee_detail.append(employee_details.objects.get(employee_id=i.employee_id))
 
-    return render(request,'symbiote/employee_details.html',{'username':request.user,'details':employee_detail})
+    return render(request, 'symbiote/employee_details.html', {'username': request.user, 'details': employee_detail})
 
 
 def attendance_status(request):
     project_id = supervisor_assign.objects.get(supervisor_username=request.user).project_id
-    today=datetime.date.today()
-    file_name = (str(today.day) + '-' + str(today.month if len(str(today.month)) == 2 else '0' + str(today.month)) + '-' + str(today.year)) + ".xlsx"
+    today = datetime.date.today()
+    file_name = (str(today.day) + '-' + str(
+        today.month if len(str(today.month)) == 2 else '0' + str(today.month)) + '-' + str(today.year)) + ".xlsx"
     df = pd.read_excel(r'symbiote/employee_assign/' + str(project_id) + '/' + file_name)
     emp_id = df['Employee_id']
     detail_name = df['Employee_name']
     detail_status = df['Status']
     details = []
-    for i in range( len(detail_name)):
+    for i in range(len(detail_name)):
         details.append([emp_id[i], detail_name[i], detail_status[i]])
-    return render(request,'symbiote/attendance_status.html',{'username': request.user,'details':details})
+    return render(request, 'symbiote/attendance_status.html', {'username': request.user, 'details': details})
 
 
 def add_employee(request):
-
-    if request.method=="POST":
+    if request.method == "POST":
         name = request.POST['name']
         age = request.POST['age']
         date_of_birth = request.POST['date_of_birth']
@@ -162,111 +165,112 @@ def add_employee(request):
         aadhar_number = request.POST['aadhar_number']
         image = request.POST.get("image")
 
-    return render(request,'symbiote/add_employee.html')
+    return render(request, 'symbiote/add_employee.html')
 
 
-
-def save_excel(request):
-    employee=details.objects.all()
-    emp_id=[]
-    for i in employee:
-        emp_id.append(i.Employee_id)
-    today_status=attendence_area.objects.filter(date=datetime.date.today())
-    late_entry=[];present=[]
-    for i in today_status:
-        time=i.Time
-        time = str((time)).split(":")
-        hh = int(time[0])
-        mm = int(time[1])
-
-        if hh > 10 or (hh == 10 and mm != 0):
-            late_entry.append(i.Employee_id)
-        else:
-            present.append(i.Employee_id)
-    lateandpresent=present+late_entry
-    absent_entry=set(emp_id)-set(lateandpresent)
-    status=[]
-    for i in emp_id:
-        if i in late_entry:
-            status.append([i,"Late"])
-        elif i in present:
-            status.append([i,"present"])
-        elif i in absent_entry:
-            status.append([i,"absent"])
-
-
-    #df = pd.DataFrame(status, columns=['Employee_Id', 'Status'])
-    date=datetime.datetime.today().date()
-    #df.to_excel("symbiote/static/symbiote/index_styles/attendance_status_files/"+(str(date) + ".xlsx"))
-    return redirect('sample')
-
-def save_clear(request):
-    print("yes")
-    employee = details.objects.all()
-    emp_id = []
-    for i in employee:
-        emp_id.append(i.Employee_id)
-    today_status = attendence_area.objects.filter(date=datetime.date.today())
-    late_entry = []
-    present = []
-    for i in today_status:
-        time = i.Time
-        time = str((time)).split(":")
-        hh = int(time[0])
-        mm = int(time[1])
-        if hh > 10 or (hh == 10 and mm != 0):
-            late_entry.append(i.Employee_id)
-        else:
-            present.append(i.Employee_id)
-    lateandpresent = present + late_entry
-    absent_entry = set(emp_id) - set(lateandpresent)
-    status = []
-    for i in emp_id:
-        if i in late_entry:
-            status.append([i, "Late"])
-        elif i in present:
-            status.append([i, "present"])
-        elif i in absent_entry:
-            status.append([i, "absent"])
-    print(status)
-
-
-    #df = pd.DataFrame(status, columns=['Employee_Id', 'Status'])
-    date=datetime.datetime.today().date()
-    #df.to_excel("symbiote/static/symbiote/index_styles/attendance_status_files/"+(str(date) + ".xlsx"))
-    data = attendence_area.objects.all()
-    data.delete()
-    return redirect('/')
+# def save_excel(request):
+#     employee=details.objects.all()
+#     emp_id=[]
+#     for i in employee:
+#         emp_id.append(i.Employee_id)
+#     today_status=attendence_area.objects.filter(date=datetime.date.today())
+#     late_entry=[];present=[]
+#     for i in today_status:
+#         time=i.Time
+#         time = str((time)).split(":")
+#         hh = int(time[0])
+#         mm = int(time[1])
+#
+#         if hh > 10 or (hh == 10 and mm != 0):
+#             late_entry.append(i.Employee_id)
+#         else:
+#             present.append(i.Employee_id)
+#     lateandpresent=present+late_entry
+#     absent_entry=set(emp_id)-set(lateandpresent)
+#     status=[]
+#     for i in emp_id:
+#         if i in late_entry:
+#             status.append([i,"Late"])
+#         elif i in present:
+#             status.append([i,"present"])
+#         elif i in absent_entry:
+#             status.append([i,"absent"])
+#
+#
+#     #df = pd.DataFrame(status, columns=['Employee_Id', 'Status'])
+#     date=datetime.datetime.today().date()
+#     #df.to_excel("symbiote/static/symbiote/index_styles/attendance_status_files/"+(str(date) + ".xlsx"))
+#     return redirect('sample')
+#
+# def save_clear(request):
+#     print("yes")
+#     employee = details.objects.all()
+#     emp_id = []
+#     for i in employee:
+#         emp_id.append(i.Employee_id)
+#     today_status = attendence_area.objects.filter(date=datetime.date.today())
+#     late_entry = []
+#     present = []
+#     for i in today_status:
+#         time = i.Time
+#         time = str((time)).split(":")
+#         hh = int(time[0])
+#         mm = int(time[1])
+#         if hh > 10 or (hh == 10 and mm != 0):
+#             late_entry.append(i.Employee_id)
+#         else:
+#             present.append(i.Employee_id)
+#     lateandpresent = present + late_entry
+#     absent_entry = set(emp_id) - set(lateandpresent)
+#     status = []
+#     for i in emp_id:
+#         if i in late_entry:
+#             status.append([i, "Late"])
+#         elif i in present:
+#             status.append([i, "present"])
+#         elif i in absent_entry:
+#             status.append([i, "absent"])
+#     print(status)
+#
+#
+#     #df = pd.DataFrame(status, columns=['Employee_Id', 'Status'])
+#     date=datetime.datetime.today().date()
+#     #df.to_excel("symbiote/static/symbiote/index_styles/attendance_status_files/"+(str(date) + ".xlsx"))
+#     data = attendence_area.objects.all()
+#     data.delete()
+#     return redirect('/')
 
 
 def download_stats(request):
-    return render(request,'symbiote/download_files.html',{"username":request.user})
+    return render(request, 'symbiote/download_files.html', {"username": request.user})
+
 
 def video_feed(request):
     return StreamingHttpResponse(gen(VideoCamera()), content_type='multipart/x-mixed-replace; boundary=frame')
 
+
 def stop_streaming(request):
-    if request.method=="POST":
+    if request.method == "POST":
         print("PRINTING")
         camera = VideoCamera()
         camera.stop_streaming()
-        return render(request,'symbiote/download_files.html' , {"status": True , "username":request.user})
-    return render(request, 'symbiote/download_files.html' , {"status": False , "username":request.user})
+        return render(request, 'symbiote/download_files.html', {"status": True, "username": request.user})
+    return render(request, 'symbiote/download_files.html', {"status": False, "username": request.user})
+
 
 def uncapture(request):
-    if request.method=="POST":
-       return redirect('stop_streaming')
+    if request.method == "POST":
+        return redirect('stop_streaming')
 
 
 def login2(request):
-    if request.method=="POST":
-        user_name=request.POST['user_name']
-        password=request.POST['password']
-        print(user_name,password)
-
+    if request.method == "POST":
+        user_name = request.POST['user_name']
+        password = request.POST['password']
+        print(user_name, password)
 
         return redirect('login2')
-    return render(request,'symbiote/login2.html')
+    return render(request, 'symbiote/login2.html')
 
 
 def logout(request):
@@ -275,33 +279,21 @@ def logout(request):
 
 
 def login_supervisor(request):
-    if request.method=="POST":
-        username=request.POST["username"]
-        password=request.POST["password"]
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
         user = authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            if len(supervisor_detail.objects.filter(username=username,password=password))!=0:
+            if len(supervisor_detail.objects.filter(username=username, password=password)) != 0:
                 if len(supervisor_assign.objects.filter(supervisor_username=username)) != 0:
                     return redirect('supervisor_page')
                 else:
-                    return render(request, 'symbiote/no_assign.html',{'username':username})
+                    return render(request, 'symbiote/no_assign.html', {'username': username})
 
             else:
                 return render(request, 'symbiote/login2.html', {'message': 'Incorrect Credientials'})
 
-        return render(request,'symbiote/login2.html',{'message':'Incorrect Credientials'})
+        return render(request, 'symbiote/login2.html', {'message': 'Incorrect Credientials'})
     else:
-        return render(request,'symbiote/login2.html',{'message':''})
-
-
-
-
-
-
-
-
-
-
-
-
+        return render(request, 'symbiote/login2.html', {'message': ''})
